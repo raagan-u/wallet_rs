ae3d3a5eac950d26eccc4902925be97a546a6191
raagan-u <n0r313n7@protonmail.ch>
2025-05-31 10:54:45 +0530

    add fee related bare fns

--- src/bitcoin_wallet/tx_builder.rs
+++ src/bitcoin_wallet/tx_builder.rs
@@ -1,5 +1,12 @@
-use bitcoin::{absolute::LockTime, transaction::Version, OutPoint, ScriptBuf, Sequence, Transaction, TxIn, TxOut, Witness};
+use std::str::FromStr;
 
+use bitcoin::{absolute::LockTime, transaction::Version, OutPoint, ScriptBuf, Sequence, Transaction, TxIn, TxOut, Txid, Witness};
+
+use crate::types::UTXO;
+
+const DUST_THRESHOLD: u64 = 546;
+
+//todo: hold UTXOs rather than TxIn
 pub struct TransactionBuilder {
     pub inputs: Vec<TxIn>,
     pub outputs: Vec<TxOut>,
@@ -15,9 +22,14 @@ impl TransactionBuilder {
 }
 
 impl TransactionBuilder {
-    pub fn add_segwit_input(&mut self, outpoint: OutPoint, sequence: Sequence) {
+    pub fn estimate_vsize(&self) -> Result<u64, Error> {
+        
+        Ok(100)
+    }
+    
+    pub fn add_input(&mut self, outpoint: UTXO, sequence: Sequence) {
         let txin = TxIn {
-            previous_output: outpoint,
+            previous_output: OutPoint { txid: Txid::from_str(&outpoint.txid).unwrap(), vout: outpoint.vout },
             script_sig: ScriptBuf::new(),
             sequence: sequence,
             witness: Witness::new()
@@ -29,6 +41,38 @@ impl TransactionBuilder {
         self.outputs.push(output);
     }
 
+    pub fn set_fee_with_fee_rate(&mut self, fee_rate: u64) -> Result<(), Error> {
+        // Calculate transaction size (you'll need to estimate/calculate vbytes)
+        let tx_vsize = self.estimate_vsize()?;
+        let calculated_fee = tx_vsize * fee_rate;
+        
+        let total_input: u64 = self.inputs.iter()
+            .map(|txin| txin.previous_output.value)
+            .sum();
+        
+        let total_output: u64 = self.outputs.iter()
+            .map(|txout| txout.value)
+            .sum();
+        
+        // Check if we have enough funds
+        if total_input < total_output + calculated_fee {
+            return Err(Error::InsufficientFunds);
+        }
+        
+        // Calculate change
+        let change_amount = total_input - total_output - calculated_fee;
+        
+        // Add change output if above dust threshold
+        if change_amount > DUST_THRESHOLD {
+            self.outputs.push(TxOut {
+                value: change_amount,
+                script_pubkey: self.change_script.clone(),
+            });
+        }
+        // If change is dust, it becomes additional fee
+        
+        Ok(())
+    }
     pub fn sign(&mut self) {
         // init the sighash cache with tx
         // fetch prevouts for amounts
